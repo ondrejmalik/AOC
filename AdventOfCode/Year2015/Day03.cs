09bed2b1f2767a49c87765b6088477e1f796c5fb
ondrejmalik <o.m777@email.cz>
2026-08-15 16:46:46 +0200

    2015 - day03;04

--- AdventOfCode/Year2015/Day03.cs
+++ AdventOfCode/Year2015/Day03.cs
@@ -7,11 +7,97 @@ public class Day03 : BaseSolver
 
     public override string SolvePart1(string[] input)
     {
-        return "Not implemented yet";
+        int x = 0;
+        int y = 0;
+        List<(int, int)> visitedLocations = new List<(int, int)>();
+        visitedLocations.Add((x, y));
+        foreach (char c in input[0])
+        {
+            switch (c)
+            {
+                case '^':
+                    y += 1;
+                    break;
+                case 'v':
+                    y -= 1;
+                    break;
+                case '>':
+                    x += 1;
+                    break;
+                case '<':
+                    x -= 1;
+                    break;
+            }
+
+            if (!visitedLocations.Contains((x, y)))
+            {
+                visitedLocations.Add((x, y));
+            }
+        }
+
+        return visitedLocations.Count().ToString();
     }
 
     public override string SolvePart2(string[] input)
     {
-        return "Not implemented yet";
+        int xSanta = 0;
+        int ySanta = 0;
+
+        int xRobo = 0;
+        int yRobo = 0;
+        List<(int, int)> visitedLocations = new List<(int, int)>();
+        visitedLocations.Add((xSanta, ySanta));
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            char c = input[0][i];
+            if (i % 2 == 0) //santa
+            {
+                switch (c)
+                {
+                    case '^':
+                        ySanta += 1;
+                        break;
+                    case 'v':
+                        ySanta -= 1;
+                        break;
+                    case '>':
+                        xSanta += 1;
+                        break;
+                    case '<':
+                        xSanta -= 1;
+                        break;
+                }
+
+                if (!visitedLocations.Contains((xSanta, ySanta)))
+                {
+                    visitedLocations.Add((xSanta, ySanta));
+                }
+            }
+            else //robo santa
+            {
+                switch (c)
+                {
+                    case '^':
+                        yRobo += 1;
+                        break;
+                    case 'v':
+                        yRobo -= 1;
+                        break;
+                    case '>':
+                        xRobo += 1;
+                        break;
+                    case '<':
+                        xRobo -= 1;
+                        break;
+                }
+
+                if (!visitedLocations.Contains((xRobo, yRobo)))
+                {
+                    visitedLocations.Add((xRobo, yRobo));
+                }
+            }
+        }
+
+        return visitedLocations.Count().ToString();
     }
 }
--- AdventOfCode/Year2015/Day04.cs
+++ AdventOfCode/Year2015/Day04.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace AdventOfCode.Year2015;
 
 public class Day04 : BaseSolver
@@ -7,7 +10,30 @@ public class Day04 : BaseSolver
 
     public override string SolvePart1(string[] input)
     {
-        return "Not implemented yet";
+        String secret = input[0];
+        StringBuilder s = new StringBuilder();
+        s.Append(secret);
+        for (int i = 0; i < Int32.MaxValue; i++)
+        {
+            s.Length = secret.Length;
+            s.Append(i.ToString());
+            byte[] data = Encoding.ASCII.GetBytes(s.ToString());
+            var a = MD5.HashData(data);
+            var b = Convert.ToHexString(a);
+            if (b[0] == '0' &&
+                b[1] == '0' &&
+                b[2] == '0' &&
+                b[3] == '0' &&
+                b[4] == '0' &&
+                b[5] == '0'
+               )
+            {
+                return i.ToString();
+            }
+        }
+
+
+        return "not found";
     }
 
     public override string SolvePart2(string[] input)
